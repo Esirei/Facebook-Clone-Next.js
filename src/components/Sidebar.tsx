4b5bd8ebb0cceffc063a57323c881b3940b681a7
Esi're <esiri92@gmail.com>
2021-05-23 21:19:18 +0100

    Make sidebar sticky.

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -6,15 +6,17 @@ import SidebarItem from './SidebarItem';
 const Sidebar = () => {
   const [session] = useSession();
   return (
-    <div className="p-2 mt-5 max-w-[360px] xl:min-w-[300px]">
-      <SidebarItem src={session.user.image} title={session.user.name} />
-      <SidebarItem Icon={UsersIcon} title="Friends" />
-      <SidebarItem Icon={UserGroupIcon} title="Groups" />
-      <SidebarItem Icon={ShoppingBagIcon} title="Marketplace" />
-      <SidebarItem Icon={DesktopComputerIcon} title="Watch" />
-      <SidebarItem Icon={CalendarIcon} title="Events" />
-      <SidebarItem Icon={ClockIcon} title="Memories" />
-      <SidebarItem Icon={ChevronDownIcon} title="See More" />
+    <div className="px-2 max-w-[360px] xl:min-w-[300px]">
+      <div className="sticky top-[72px] py-2 sm:py-4">
+        <SidebarItem src={session.user.image} title={session.user.name} />
+        <SidebarItem Icon={UsersIcon} title="Friends" />
+        <SidebarItem Icon={UserGroupIcon} title="Groups" />
+        <SidebarItem Icon={ShoppingBagIcon} title="Marketplace" />
+        <SidebarItem Icon={DesktopComputerIcon} title="Watch" />
+        <SidebarItem Icon={CalendarIcon} title="Events" />
+        <SidebarItem Icon={ClockIcon} title="Memories" />
+        <SidebarItem Icon={ChevronDownIcon} title="See More" />
+      </div>
     </div>
   );
 };
